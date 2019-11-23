a6d1709afaa46009ab669a70dbf76fec8ff4936c
Benjamin Dehli <superelg@gmail.com>
2019-11-23 21:28:11 +0100

    Add module export for portfolio data

---
+++ src/data/portfolio.js
@@ -0,0 +1,53 @@
+import ainaWassvikDauntless from './releases/data/ainaWassvik_dauntless_0u0XnmGmw3WF99J0jMXyP3.json';
+import changemakersVeidemannen from './releases/data/changemakers_veidemannen_7brelOFhq6xXGWExXPwcuw.json';
+import confusionConfusion from './releases/data/confusion_confusion_3k9hYBU4jbh2divQo8vauI.json';
+import confusionEscapeAndExplore from './releases/data/confusion_escapeExplore_0CxN1FYtoJD71q3z3jaU3v.json';
+import confusionFadeAway from './releases/data/confusion_fadeAway_5kskx9GuawhjiDzwD8qtmd.json';
+import confusionItsAllToClear from './releases/data/confusion_itsAllTooClear_1Z3Rj0CzsAhtobNPYVDcke.json';
+import confusionLetTheGamesBegin from './releases/data/confusion_thisTime_2Lr467UefH9pjf3KP2deAL.json';
+import confusionThisTime from './releases/data/confusion_lettheGamesBegin_4XEL4eIvtDQ4plgFKtyu1t.json';
+import hauntedBySilhouettesDeadlock from './releases/data/hauntedBySilhouettes_deadlock_7fVdrI4oTPXduyZiZNlcCH.json';
+import hauntedBySilhouettesTheLastDayOnEarth from './releases/data/hauntedBySilhouettes_theLastDayonEarth_5Ea891yomzVe9qs5e2DXMJ.json';
+import hauntedBySilhouettesUndertow from './releases/data/hauntedBySilhouettes_undertow_1rTLnAzETljGUZHk7aoetP.json';
+import setOnesCapDontYouWannaKnow from './releases/data/setOnesCap_dontYouWannaKnow_6I5ZjNZ9P7mS56sEuP5QzU.json';
+import setOnesCapLikeAStone from './releases/data/setOnesCap_likeaStone_0uiT2eVvUf9zRDs2Jx6RES.json';
+import setOnesCapWaitForTheOther from './releases/data/setOnesCap_waitfortheOther_7o3Zv2B9uDTLlykNW5EEOC.json';
+import spinningsWheelsBandPowerOfAGrin from './releases/data/spinningWheelsBand_powerofaGrin_3frJWthdoCi4OyLnDUh4j0.json';
+import spinningsWheelsBandBackWhen from './releases/data/spinningWheelsBandRandleCurrieJustinWilliamso_backWhen_7tW3Ik76uWSXsksLoheDrr.json';
+import theWhiteDevilGetLucky from './releases/data/theWhiteDevil_getLucky_3WQblBc1s04myN7D7M3x2M.json';
+import wildWhensBacktrackJack from './releases/data/wildWhens_backtrackJack_2qdN2vUuvcryyN9WeT2EP9.json';
+import wildWhensChainsOfYesterday from './releases/data/wildWhens_chainsofYesterday_2gQwPWaqLb7bS7PjNuLppZ.json';
+import wildWhensCrazyToTheBone from './releases/data/wildWhens_crazytotheBone_0iZwHC4UX76xpFv10Dr6i0.json';
+import wildWhensCrazyToTheBoneRemastered from './releases/data/wildWhens_crazytotheBone_3Ix2e8pg7TRiDuKqaQltGh.json';
+import wildWhensFromTheValley from './releases/data/wildWhens_fromtheValley_3usd6T446qaQiVSVRuHvPQ.json';
+import wildWhensRebelHeart from './releases/data/wildWhens_rebelHeart_7vIxzySCGMK2MYQESmDXbl.json';
+import wildWhensSailinSarah from './releases/data/wildWhens_sailinSarah_50PONsFQZadgxvPJclRLjv.json';
+import wildWhensTravellinMan from './releases/data/wildWhens_travellinMan_2kO379DWolHqoC4lrlgToK.json';
+
+export default [
+  ainaWassvikDauntless,
+  changemakersVeidemannen,
+  confusionConfusion,
+  confusionEscapeAndExplore,
+  confusionFadeAway,
+  confusionItsAllToClear,
+  confusionLetTheGamesBegin,
+  confusionThisTime,
+  hauntedBySilhouettesDeadlock,
+  hauntedBySilhouettesTheLastDayOnEarth,
+  hauntedBySilhouettesUndertow,
+  setOnesCapDontYouWannaKnow,
+  setOnesCapLikeAStone,
+  setOnesCapWaitForTheOther,
+  spinningsWheelsBandPowerOfAGrin,
+  spinningsWheelsBandBackWhen,
+  theWhiteDevilGetLucky,
+  wildWhensBacktrackJack,
+  wildWhensChainsOfYesterday,
+  wildWhensCrazyToTheBone,
+  wildWhensCrazyToTheBoneRemastered,
+  wildWhensFromTheValley,
+  wildWhensRebelHeart,
+  wildWhensSailinSarah,
+  wildWhensTravellinMan
+]
