5a69ef0885928e9844c9513b7bda48071c1a29eb
Benjamin Dehli <superelg@gmail.com>
2019-12-05 22:44:24 +0100

    Add component for artists

---
+++ src/components/partials/Portfolio/Artist.js
@@ -0,0 +1,59 @@
+// Dependencies
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+
+// Components
+import {withFirebase} from '../../Firebase';
+import Release from './Release';
+
+// Stylesheets
+import style from './Artist.module.scss';
+
+class Artist extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      releases: {}
+    };
+  }
+
+  componentDidMount() {
+    this.getReleasesByArtist(this.props.artist.id);
+  }
+
+  getReleasesByArtist(artistId) {
+    this.props.firebase.getReleasesByArtistId(artistId).then(releasesByArtist => {
+      this.setState({releases: releasesByArtist})
+    });
+  }
+
+  renderReleases(artist) {
+    return this.state.releases && this.state.releases.length
+      ? this.state.releases.map(release => {
+        return <Release key={release.id} release={release} artist={artist} viewType={this.props.viewType}/>
+      })
+      : '';
+  }
+
+  render() {
+    return (<React.Fragment>
+      {this.renderReleases(this.props.artist)}
+    </React.Fragment>);
+  }
+}
+
+Artist.propTypes = {
+  artist: PropTypes.object.isRequired,
+  viewType: PropTypes.string
+};
+
+Release.defaultProps = {
+  viewType: 'list'
+}
+
+const mapStateToProps = null;
+
+const mapDispatchToProps = null;
+
+export default connect(mapStateToProps, mapDispatchToProps)(withFirebase(Artist));
