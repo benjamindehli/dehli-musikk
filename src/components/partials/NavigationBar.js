d188be9870101434ceecb4345de33f69b5778910
Benjamin Dehli <superelg@gmail.com>
2019-12-07 16:24:07 +0100

    Add sidebar menu to navigation bar

--- src/components/partials/NavigationBar.js
+++ src/components/partials/NavigationBar.js
@@ -11,17 +11,66 @@ import {ReactComponent as MenuIcon} from '../../assets/svg/menuIcon.svg'
 import style from './NavigationBar.module.scss';
 
 class NavigationBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showSidebar: false,
+      hidingSidebar: false
+    };
+    this.setWrapperRef = this.setWrapperRef.bind(this);
+    this.handleClickOutside = this.handleClickOutside.bind(this);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this.handleClickOutside);
+  }
+
+  setWrapperRef(node) {
+    this.wrapperRef = node;
+  }
+
+  handleShowSidebarClick() {
+    this.setState({showSidebar: true});
+  }
+
+  handleClickOutside(event) {
+    if (this.wrapperRef && !this.wrapperRef.contains(event.target) && this.state.showSidebar) {
+      this.hideSidebar();
+    }
+  }
+
+  hideSidebar() {
+    this.setState({hidingSidebar: true});
+    setTimeout(function() {
+      this.setState({showSidebar: false, hidingSidebar: false});
+    }.bind(this), 225);
+  }
+
+  componentDidMount() {
+    document.addEventListener('mousedown', this.handleClickOutside);
+  }
 
   render() {
     return (<div className={style.navigationBar}>
-      <span className={style.menuButton}>
+      <span onClick={() => this.handleShowSidebarClick()} className={style.menuButton}>
         <MenuIcon className={style.menuIcon}/>
       </span>
-      <Link to='/' aria-label='Link to Dehli Musikk home page'>
-        <span className={style.appLogo}>
-          <DehliMusikkLogo alt="Dehli Musikk logo"/>
-        </span>
-      </Link>
+      <div className={`${style.sidebarOverlay} ${this.state.showSidebar
+          ? style.active
+          : ''} ${this.state.hidingSidebar
+            ? style.hidingSidebar
+            : ''} `}>
+        <div ref={this.setWrapperRef} className={style.sidebarContent}>
+          <div className={style.sidebarContentHeader}>
+            <Link to='/' aria-label='Link to Dehli Musikk home page'>
+              <span className={style.appLogo}>
+                <DehliMusikkLogo alt="Dehli Musikk logo"/>
+              </span>
+            </Link>
+          </div>
+          Sidebar
+        </div>
+      </div>
     </div>)
   }
 }
