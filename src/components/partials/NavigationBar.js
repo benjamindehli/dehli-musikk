51c3170219843fb9a17b58cd179e48b873e2c8b0
Benjamin Dehli <superelg@gmail.com>
2019-12-07 18:57:20 +0100

    Add language selector to navigation bar

--- src/components/partials/NavigationBar.js
+++ src/components/partials/NavigationBar.js
@@ -15,27 +15,50 @@ class NavigationBar extends Component {
     super(props);
     this.state = {
       showSidebar: false,
-      hidingSidebar: false
+      hidingSidebar: false,
+      showLanguageSelectorList: false
     };
-    this.setWrapperRef = this.setWrapperRef.bind(this);
-    this.handleClickOutside = this.handleClickOutside.bind(this);
+    this.setSidebarWrapperRef = this.setSidebarWrapperRef.bind(this);
+    this.setLanguageSelectorListWrapperRef = this.setLanguageSelectorListWrapperRef.bind(this);
+    this.handleClickOutsideSidebar = this.handleClickOutsideSidebar.bind(this);
+    this.handleClickOutsideLanguageSelectorList = this.handleClickOutsideLanguageSelectorList.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener('mousedown', this.handleClickOutsideSidebar);
+    document.addEventListener('mousedown', this.handleClickOutsideLanguageSelectorList);
   }
 
   componentWillUnmount() {
-    document.removeEventListener('mousedown', this.handleClickOutside);
+    document.removeEventListener('mousedown', this.handleClickOutsideSidebar);
+    document.removeEventListener('mousedown', this.handleClickOutsideLanguageSelectorList);
   }
 
-  setWrapperRef(node) {
-    this.wrapperRef = node;
+  setSidebarWrapperRef(node) {
+    this.sidebarWrapperRef = node;
+  }
+
+  setLanguageSelectorListWrapperRef(node) {
+    this.languageSelectorListWrapperRef = node;
   }
 
   handleShowSidebarClick() {
     this.setState({showSidebar: true});
   }
 
-  handleClickOutside(event) {
-    if (this.wrapperRef && !this.wrapperRef.contains(event.target) && this.state.showSidebar) {
+  handleShowLanguageSelectorList() {
+    this.setState({showLanguageSelectorList: true});
+  }
+
+  handleClickOutsideSidebar(event) {
+    if (this.sidebarWrapperRef && !this.sidebarWrapperRef.contains(event.target) && this.state.showSidebar) {
       this.hideSidebar();
+    }
+  }
+
+  handleClickOutsideLanguageSelectorList(event) {
+    if (this.languageSelectorListWrapperRef && !this.languageSelectorListWrapperRef.contains(event.target) && this.state.showLanguageSelectorList) {
+      this.hideLanguageSelectorList();
     }
   }
 
@@ -46,8 +69,37 @@ class NavigationBar extends Component {
     }.bind(this), 225);
   }
 
-  componentDidMount() {
-    document.addEventListener('mousedown', this.handleClickOutside);
+  hideLanguageSelectorList() {
+    this.setState({showLanguageSelectorList: false});
+  }
+
+  renderLanguageSelectorButton(availableLanguages, selectedLanguageKey) {
+    const hasAvailableLanguages = availableLanguages && Object.keys(availableLanguages).length;
+    if (hasAvailableLanguages) {
+      const selectedLanguage = availableLanguages[selectedLanguageKey];
+      return (<span>{selectedLanguage.name}</span>)
+    } else
+      return '';
+    }
+
+  renderLanguageSelectorList(availableLanguages, multilingualRoutes, selectedLanguageKey) {
+    const hasAvailableLanguages = availableLanguages && Object.keys(availableLanguages).length;
+    const hasMultilingualRoutes = multilingualRoutes && Object.keys(multilingualRoutes).length;
+    if (hasAvailableLanguages && hasMultilingualRoutes) {
+      const languageElements = Object.keys(availableLanguages).map(languageKey => {
+        const language = availableLanguages[languageKey];
+        const path = multilingualRoutes[languageKey].path;
+        const isActive = languageKey === selectedLanguageKey;
+        return (<li key={languageKey}>
+          <a href={path} className={isActive
+              ? style.activeLink
+              : ''}>{language.name}</a>
+        </li>)
+      });
+      return (<ul>{languageElements}</ul>);
+    } else {
+      return '';
+    }
   }
 
   render() {
@@ -55,12 +107,20 @@ class NavigationBar extends Component {
       <span onClick={() => this.handleShowSidebarClick()} className={style.menuButton}>
         <MenuIcon className={style.menuIcon}/>
       </span>
+      <div>
+        <span onClick={() => this.handleShowLanguageSelectorList()}>{this.renderLanguageSelectorButton(this.props.availableLanguages, this.props.selectedLanguageKey)}</span>
+        <div ref={this.setLanguageSelectorListWrapperRef} className={`${style.languageSelectorList} ${this.state.showLanguageSelectorList
+            ? style.active
+            : ''}`}>
+          {this.renderLanguageSelectorList(this.props.availableLanguages, this.props.multilingualRoutes, this.props.selectedLanguageKey)}
+        </div>
+      </div>
       <div className={`${style.sidebarOverlay} ${this.state.showSidebar
           ? style.active
           : ''} ${this.state.hidingSidebar
             ? style.hidingSidebar
             : ''} `}>
-        <div ref={this.setWrapperRef} className={style.sidebarContent}>
+        <div ref={this.setSidebarWrapperRef} className={style.sidebarContent}>
           <div className={style.sidebarContentHeader}>
             <Link to='/' aria-label='Link to Dehli Musikk home page'>
               <span className={style.appLogo}>
@@ -68,14 +128,13 @@ class NavigationBar extends Component {
               </span>
             </Link>
           </div>
-          Sidebar
         </div>
       </div>
     </div>)
   }
 }
 
-const mapStateToProps = null;
+const mapStateToProps = state => ({availableLanguages: state.availableLanguages, multilingualRoutes: state.multilingualRoutes, selectedLanguageKey: state.selectedLanguageKey});
 
 const mapDispatchToProps = null;
 
