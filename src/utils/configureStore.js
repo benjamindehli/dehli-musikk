80f87fe10ada6030149b6aec9522ead33528fe87
Benjamin Dehli <superelg@gmail.com>
2019-12-08 20:08:46 +0100

    Disable redux dev tools in production

--- src/utils/configureStore.js
+++ src/utils/configureStore.js
@@ -1,7 +1,7 @@
 import { createBrowserHistory } from 'history';
 import { routerMiddleware } from 'connected-react-router';
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import thunk from 'redux-thunk';
 import createRootReducer from '../reducers';
 
