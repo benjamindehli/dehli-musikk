25ad2a855561bd167074c6d2a8ebdba99a6f30c5
Benjamin Dehli <superelg@gmail.com>
2019-12-10 22:58:01 +0100

    Add logic and style rules for breadcrumbs component

--- src/components/partials/Breadcrumbs.js
+++ src/components/partials/Breadcrumbs.js
@@ -2,20 +2,62 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
+import {Helmet} from 'react-helmet-async';
 
 import style from './Breadcrumbs.module.scss';
 
-export class Breadcrumb extends Component {
+export class Breadcrumbs extends Component {
+
+  renderBreadcrumbJsonLd(breadcrumbs) {
+    const jsonLd = {
+      "@context": "https://schema.org",
+      "@type": "BreadcrumbList",
+      "itemListElement": [
+        {
+          "@type": "ListItem",
+          "position": 1,
+          "item": {
+            "@id": "https://www.dehlimusikk.no",
+            "name": "Dehli Musikk"
+          }
+        }
+      ]
+    }
+    breadcrumbs.forEach((breadcrumb, index) => {
+      jsonLd.itemListElement.push({
+        "@type": "ListItem",
+        "position": index + 2,
+        "item": {
+          "@id": window.location.origin + breadcrumb.path,
+          "name": breadcrumb.name
+        }
+      })
+    })
+    return (<Helmet>
+      <script type="application/ld+json">{`${JSON.stringify(jsonLd)}`}</script>
+    </Helmet>)
+  }
+
+  renderBreadcrumbListElements(breadcrumbs) {
+    return breadcrumbs.map((breadcrumb, key) => {
+      return key === breadcrumbs.length - 1
+        ? (<li key={key}>
+          <span>{breadcrumb.name}</span>
+        </li>)
+        : (<li key={key}>
+          <Link to={breadcrumb.path}>{breadcrumb.name}</Link>
+        </li>);
+    })
+  }
 
   renderBreadcrumb() {
-    return (<div className={style.breadCrumb}>
+    return (<div className={style.breadcrumbs}>
+      {this.renderBreadcrumbJsonLd(this.props.breadcrumbs)}
       <ul>
         <li>
           <Link to={'/'}>Dehli Musikk</Link>
         </li>
-        <li>
-          <span>{this.props.pageTitle}</span>
-        </li>
+        {this.renderBreadcrumbListElements(this.props.breadcrumbs)}
       </ul>
     </div>)
   }
@@ -25,8 +67,12 @@ export class Breadcrumb extends Component {
   }
 }
 
-Breadcrumb.propTypes = {
-  pageTitle: PropTypes.string
+Breadcrumbs.propTypes = {
+  breadcrumbs: PropTypes.array
 };
 
-export default connect(null, null)(Breadcrumb);
+Breadcrumbs.defaultProps = {
+  breadcrumbs: []
+};
+
+export default connect(null, null)(Breadcrumbs);
