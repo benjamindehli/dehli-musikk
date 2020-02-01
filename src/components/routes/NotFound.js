2785525ff20e7c3249f2c1b23e08bb97c28d5bd8
Benjamin Dehli <superelg@gmail.com>
2020-02-01 23:12:42 +0100

    Add noindex tag for robots to 404 pages

--- src/components/routes/NotFound.js
+++ src/components/routes/NotFound.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Helmet} from 'react-helmet';
 
+// Stylesheets
 import style from 'components/routes/NotFound.module.scss';
 
 class NotFound extends Component {
@@ -10,6 +11,7 @@ class NotFound extends Component {
     return (<div className={style.contentSection}>
       <Helmet>
         <title>404 - Siden finnes ikke - Dehli Musikk</title>
+        <meta name="ROBOTS" content="NOINDEX"/>
       </Helmet>
       <h1>404 - Siden finnes ikke</h1>
     </div>)
