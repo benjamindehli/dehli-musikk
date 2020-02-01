8aae549b3cd368812e601967251b2a5af2ccf4a0
Benjamin Dehli <superelg@gmail.com>
2020-02-01 18:04:52 +0100

    Add null check to URL helper

--- src/helpers/urlFormatter.js
+++ src/helpers/urlFormatter.js
@@ -8,22 +8,26 @@ const replaceAndAddSpace = (string, replace, replaceWith) => {
 }
 
 export const convertToUrlFriendlyString = string => {
-  // To lower case
-  string = string.toLowerCase();
+  if (string) {
+    // To lower case
+    string = string.toLowerCase();
 
-  // Character replace
-  string = replaceAndAddSpace(string, "&", "and");
-  string = replaceAndAddSpace(string, "+", "plus");
-  string = string.replace("æ", "ae");
-  string = string.replace("ø", "oe");
-  string = string.replace("å", "aa");
+    // Character replace
+    string = replaceAndAddSpace(string, "&", "and");
+    string = replaceAndAddSpace(string, "+", "plus");
+    string = string.replace("æ", "ae");
+    string = string.replace("ø", "oe");
+    string = string.replace("å", "aa");
 
-  // Whitespace replace
-  string = string.replace(" - ", "-");
-  string = string.replace(/[\s]+/g, "-");
+    // Whitespace replace
+    string = string.replace(" - ", "-");
+    string = string.replace(/[\s]+/g, "-");
 
-  // Unwated character replace
-  string = string.replace(/[^a-z0-9-]+/ig, "");
+    // Unwated character replace
+    string = string.replace(/[^a-z0-9-]+/ig, "");
 
-  return string;
+    return string;
+  } else {
+    return '';
+  }
 }
