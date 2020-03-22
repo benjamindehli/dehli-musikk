ca63eda9816167b490aa435c5fd941e03fdc322c
Benjamin Dehli <superelg@gmail.com>
2020-03-22 23:24:22 +0100

    Remove unused imports

--- src/components/partials/NavigationBar/SearchField.js
+++ src/components/partials/NavigationBar/SearchField.js
@@ -1,7 +1,6 @@
 // Dependencies
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {Link, NavLink} from 'react-router-dom';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 // Actions
