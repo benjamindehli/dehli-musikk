a250e43c7af0ebd6a9b89f8dea495a11663b8f76
Benjamin Dehli <superelg@gmail.com>
2020-03-22 23:30:55 +0100

    Open Facebook link in new tab

--- src/components/partials/IntroContent.js
+++ src/components/partials/IntroContent.js
@@ -13,13 +13,13 @@ class IntroContent extends Component {
 
     const norwegianContent = (<React.Fragment>
       <p>Dehli Musikk tilbyr spilling av tangentinstrumenter på låter for artister og band.</p>
-      <p>Har du en låt som skal spilles inn og mangler tangeter, ta gjerne kontakt på <a href={facebookLink} title="Dehli Musikks Facebook-side">Facebook</a> eller <a href={emailLink} title="Send Dehli Musikk en e-post">e-post</a>.</p>
+      <p>Har du en låt som skal spilles inn og mangler tangeter, ta gjerne kontakt på <a href={facebookLink} title="Dehli Musikks Facebook-side" target='_blank' rel="noopener noreferrer">Facebook</a> eller <a href={emailLink} title="Send Dehli Musikk en e-post">e-post</a>.</p>
       <p>Sjekk ut min <Link to={`/${this.props.getLanguageSlug(this.props.selectedLanguageKey)}portfolio/`} title="Dehli Musikks portefølje">portefølje</Link> om du vil høre utgivelser jeg har bidratt på.</p>
     </React.Fragment>);
 
     const englishContent = (<React.Fragment>
       <p>Dehli Musikk offers playing of keyboard instruments on recordings for artists and bands.</p>
-      <p>If you're recording a song and want some keyboard instrument tracks, feel free to contact me on  <a href={facebookLink} title="Dehli Musikk's Facebook page">Facebook</a> or <a href={emailLink} title="Send Dehli Musikk an email">email</a>.</p>
+      <p>If you're recording a song and want some keyboard instrument tracks, feel free to contact me on  <a href={facebookLink} title="Dehli Musikk's Facebook page" target='_blank' rel="noopener noreferrer">Facebook</a> or <a href={emailLink} title="Send Dehli Musikk an email">email</a>.</p>
       <p>Check out my <Link to={`/${this.props.getLanguageSlug(this.props.selectedLanguageKey)}portfolio/`} title="Dehli Musikk's portfolio">portfolio</Link> if you want to hear releases where I've contributed.</p>
     </React.Fragment>);
     return (<div>
