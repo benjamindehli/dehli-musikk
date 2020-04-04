53e8e8bff20d8f7ddedae443d411cde3c493c709
Benjamin Dehli <superelg@gmail.com>
2020-04-04 13:36:27 +0200

    Replace sw-precache with workbox

---
+++ workbox-config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  // a directory should be the same as "reactSnap.destination",
+  // which default value is `build`
+  globDirectory: "build/",
+  globPatterns: [
+    "static/css/*.css",
+    "static/js/*.js",
+    "static/media/*.webp",
+    "static/media/*.jpg",
+    "shell.html",
+    "index.html"
+  ],
+  // there is "reactSnap.include": ["/shell.html"] in package.json
+  navigateFallback: "/shell.html",
+  // Ignores URLs starting from /__ (useful for Firebase):
+  // https://github.com/facebookincubator/create-react-app/issues/2237#issuecomment-302693219
+  navigateFallbackAllowlist: [/^(?!\/__).*/],
+  // configuration specific to this experiment
+  runtimeCaching: [
+    {
+      urlPattern: /\.(?:png|jpg|jpeg|svg|webp)$/,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'images'
+      }
+    },
+    {
+      urlPattern: /api/,
+      handler: "StaleWhileRevalidate"
+    }
+  ],
+  swDest: 'build/service-worker.js',
+  globIgnores: [
+    "../workbox-config.js"
+  ]
+};
