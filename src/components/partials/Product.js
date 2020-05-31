19cc5f94827f930169cf9b28bbd655830124bc12
Benjamin Dehli <superelg@gmail.com>
2020-05-31 13:31:00 +0200

    Add components for products

---
+++ src/components/partials/Product.js
@@ -0,0 +1,245 @@
+// Dependencies
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {Helmet} from 'react-helmet';
+import {Link} from 'react-router-dom';
+
+// Actions
+import {getLanguageSlug} from 'actions/LanguageActions';
+
+// Helpers
+import {getPrettyDate} from 'helpers/dateFormatter';
+import {convertToUrlFriendlyString} from 'helpers/urlFormatter'
+
+// Components
+import Button from 'components/partials/Button';
+
+// Template
+import ListItemThumbnail from 'components/template/List/ListItem/ListItemThumbnail';
+import ListItemContent from 'components/template/List/ListItem/ListItemContent';
+import ListItemContentHeader from 'components/template/List/ListItem/ListItemContent/ListItemContentHeader';
+import ListItemContentBody from 'components/template/List/ListItem/ListItemContent/ListItemContentBody';
+import ListItemActionButtons from 'components/template/List/ListItem/ListItemActionButtons';
+
+
+class Product extends Component {
+
+  renderProductSnippet(product, productId, productThumbnailSrc) {
+    const productDate = new Date(product.timestamp).toISOString();
+    const selectedLanguageKey = this.props.selectedLanguageKey
+      ? this.props.selectedLanguageKey
+      : 'no';
+    const snippet = {
+      "@context": "http://schema.org",
+      "@type": "Product",
+      "@id": `https://www.dehlimusikk.no/${this.props.getLanguageSlug(selectedLanguageKey)}products/${productId}/`,
+      "url": `https://www.dehlimusikk.no/${this.props.getLanguageSlug(selectedLanguageKey)}products/${productId}/`,
+      "description": product.content[selectedLanguageKey],
+      "brand": {
+        "@context": "http://schema.org",
+        "@type": "Organization",
+        "address": {
+          "@type": "PostalAddress",
+          "addressLocality": "Bø i Telemark",
+          "postalCode": "3804",
+          "streetAddress": "Margretes veg 15",
+          "addressCountry": {
+            "name": "NO"
+          }
+        },
+        "hasPos": {
+          "@type": "Place",
+          "address": {
+            "@type": "PostalAddress",
+            "addressLocality": "Bø i Telemark",
+            "postalCode": "3804",
+            "streetAddress": "Margretes veg 15",
+            "addressCountry": {
+              "name": "NO"
+            }
+          },
+          "hasMap": "https://www.google.com/maps?cid=13331960642102658320"
+        },
+        "location": {
+          "@type": "PostalAddress",
+          "addressLocality": "Bø i Telemark, Norway",
+          "postalCode": "3804",
+          "streetAddress": "Margretes veg 15"
+        },
+        "foundingDate": "	2019-10-01",
+        "foundingLocation": {
+          "@type": "Place",
+          "address": {
+            "@type": "PostalAddress",
+            "addressLocality": "Bø i Telemark",
+            "postalCode": "3804",
+            "streetAddress": "Margretes veg 15",
+            "addressCountry": {
+              "name": "NO"
+            }
+          },
+          "hasMap": "https://www.google.com/maps?cid=13331960642102658320"
+        },
+        "logo": {
+          "@type": "ImageObject",
+          "url": "https://www.dehlimusikk.no/DehliMusikkLogo.png"
+        },
+        "image": {
+          "@type": "ImageObject",
+          "url": "https://www.dehlimusikk.no/DehliMusikkLogo.png"
+        },
+        "email": "superelg(at)gmail.org",
+        "founder": {
+          "@type": "Person",
+          "name": "Benjamin Dehli",
+          "address": {
+            "@type": "PostalAddress",
+            "addressLocality": "Bø i Telemark",
+            "postalCode": "3804",
+            "streetAddress": "Margretes veg 15",
+            "addressCountry": {
+              "name": "NO"
+            }
+          }
+        },
+        "name": "Dehli Musikk",
+        "telephone": "+47 92 29 27 19",
+        "url": "https://www.dehlimusikk.no"
+      },
+      "productionDate": productDate,
+      "releaseDate": productDate,
+      "name": product.title,
+      "image": {
+        "@type": "ImageObject",
+        "url": `https://www.dehlimusikk.no${productThumbnailSrc}`,
+        "caption": product.title,
+        "description": product.thumbnailDescription,
+        "uploadDate": productDate,
+        "contentLocation": {
+          "name": "Dehli Musikk",
+          "address": {
+            "@type": "PostalAddress",
+            "addressLocality": "Bø i Telemark",
+            "postalCode": "3804",
+            "streetAddress": "Margretes veg 15",
+            "addressCountry": {
+              "name": "NO"
+            }
+          }
+        }
+      },
+      "offers": {
+        "@type": "Offer",
+        "price": product.price,
+        "priceCurrency": product.priceCurrency,
+        "url": product.links.shop.url,
+        "availability": "http://schema.org/OnlineOnly",
+        "validFrom" : productDate,
+      },
+      "mainEntityOfPage": {
+        "@type": "WebPage",
+        "@id": "https://www.dehlimusikk.no"
+      }
+    }
+    return (<Helmet>
+      <script type="application/ld+json">{`${JSON.stringify(snippet)}`}</script>
+    </Helmet>)
+  }
+
+  renderProductThumbnail(image, altText, fullscreen, productDate) {
+    const copyrightString = `cc-by ${productDate.getFullYear()} Benjamin Dehli dehlimusikk.no`;
+    const imageSize = fullscreen
+      ? '540px'
+      : '350px';
+    return (<React.Fragment>
+      <source sizes={imageSize} srcSet={`${image.webp55} 55w, ${image.webp350} 350w, ${image.webp540} 540w`} type="image/webp"/>
+      <source sizes={imageSize} srcSet={`${image.jpg55} 55w, ${image.jpg350} 350w, ${image.jpg540} 540w`} type="image/jpg"/>
+      <img loading="lazy" src={image.jpg350} width="350" height="260" alt={altText} copyright={copyrightString} />
+    </React.Fragment>);
+  }
+
+  renderLink(link) {
+    return link.internal
+    ? (<Link to={`/${this.props.getLanguageSlug(this.props.selectedLanguageKey)}${link.url}`} title={link.text[this.props.selectedLanguageKey]}>
+        <Button buttontype='minimal'>
+          {link.text[this.props.selectedLanguageKey]}
+        </Button>
+      </Link>)
+    : (<a href={link.url} target="_blank" rel="noopener noreferrer" title={link.text[this.props.selectedLanguageKey]}>
+        <Button buttontype='minimal'>
+          {link.text[this.props.selectedLanguageKey]}
+        </Button>
+      </a>);
+  }
+
+  render() {
+    const selectedLanguageKey = this.props.selectedLanguageKey
+      ? this.props.selectedLanguageKey
+      : 'no';
+    const product = this.props.product;
+    const productId = convertToUrlFriendlyString(product.title);
+
+    const imagePathWebp = `data/products/thumbnails/web/webp/${productId}`;
+    const imagePathJpg = `data/products/thumbnails/web/jpg/${productId}`;
+    const image = {
+      webp55: require(`../../${imagePathWebp}_55.webp`),
+      webp350: require(`../../${imagePathWebp}_350.webp`),
+      webp540: require(`../../${imagePathWebp}_540.webp`),
+      jpg55: require(`../../${imagePathJpg}_55.jpg`),
+      jpg350: require(`../../${imagePathJpg}_350.jpg`),
+      jpg540: require(`../../${imagePathJpg}_540.jpg`)
+    };
+    const productDate = new Date(product.timestamp);
+    const productPath = `/${this.props.getLanguageSlug(this.props.selectedLanguageKey)}products/${productId}/`;
+
+    const link = {
+      to: productPath,
+      title: product.title
+    };
+
+    return product && product.content && product.content[selectedLanguageKey]
+      ? (<React.Fragment>
+        {this.renderProductSnippet(product, productId, image.jpg540)}
+        <ListItemThumbnail fullscreen={this.props.fullscreen} link={link}>
+          {this.renderProductThumbnail(image, product.thumbnailDescription, this.props.fullscreen, productDate)}
+        </ListItemThumbnail>
+        <ListItemContent fullscreen={this.props.fullscreen}>
+            <ListItemContentHeader fullscreen={this.props.fullscreen} link={link}>
+              {
+                this.props.fullscreen
+                  ? (<h2>{product.title}</h2>)
+                  : (<Link to={productPath} title={product.title}>
+                    <h2>{product.title}</h2>
+                  </Link>)
+              }
+              <time dateTime={productDate.toISOString()}>
+                {getPrettyDate(productDate, selectedLanguageKey)}
+              </time>
+            </ListItemContentHeader>
+            <ListItemContentBody fullscreen={this.props.fullscreen}>
+              {
+                product.content[selectedLanguageKey].split('\n').map((paragraph, key) => {
+                  return (<p key={key}>{paragraph}</p>)
+                })
+              }
+            </ListItemContentBody>
+            <ListItemActionButtons fullscreen={this.props.fullscreen}>
+            {
+              product.link && this.props.fullscreen
+                ? this.renderLink(product.link)
+                : ''
+            }
+          </ListItemActionButtons>
+        </ListItemContent>
+      </React.Fragment>)
+      : '';
+  }
+}
+
+const mapStateToProps = state => ({selectedLanguageKey: state.selectedLanguageKey});
+
+const mapDispatchToProps = {
+  getLanguageSlug
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Product);
---
+++ src/components/routes/Products.js
@@ -0,0 +1,258 @@
+// Dependencies
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {Helmet} from 'react-helmet';
+import {Redirect} from 'react-router-dom';
+
+// Components
+import Product from 'components/partials/Product';
+import Breadcrumbs from 'components/partials/Breadcrumbs';
+import Modal from 'components/partials/Modal';
+
+// Template
+import Container from 'components/template/Container';
+import List from 'components/template/List';
+import ListItem from 'components/template/List/ListItem';
+
+// Actions
+import {getLanguageSlug, updateMultilingualRoutes, updateSelectedLanguageKey} from 'actions/LanguageActions';
+
+// Helpers
+import {convertToUrlFriendlyString} from 'helpers/urlFormatter'
+
+// Data
+import products from 'data/products';
+
+
+class Products extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      redirect: null
+    };
+  }
+
+  initLanguage() {
+    const selectedProductId = this.props.match && this.props.match.params && this.props.match.params.productId
+      ? this.props.match.params.productId
+      : null;
+    this.props.updateMultilingualRoutes(
+      selectedProductId
+      ? `products/${selectedProductId}/`
+      : 'products/');
+    const selectedLanguageKey = this.props.match && this.props.match.params && this.props.match.params.selectedLanguage
+      ? this.props.match.params.selectedLanguage
+      : 'no';
+    if (selectedLanguageKey !== this.props.selectedLanguageKey) {
+      this.props.updateSelectedLanguageKey(selectedLanguageKey);
+    }
+  }
+
+  componentDidMount() {
+    this.initLanguage();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.state.redirect) {
+      this.setState({redirect: null});
+    }
+    if (this.props.location.pathname !== prevProps.location.pathname){
+      this.initLanguage();
+    }
+  }
+
+  renderProducts() {
+    return products && products.length
+      ? products.map(product => {
+        return (<ListItem key={product.id} fullscreen={this.props.fullscreen}>
+          <Product product={product}/>
+        </ListItem>)
+      })
+      : '';
+  }
+
+  renderSelectedProduct(selectedProduct) {
+    const handleClickOutside = () => {
+      this.setState({
+        redirect: `/${this.props.getLanguageSlug(this.props.selectedLanguageKey)}products/`
+      });
+    }
+    const handleClickArrowLeft = selectedProduct && selectedProduct.previousProductId ? () => {
+      this.setState({
+        redirect: `/${this.props.getLanguageSlug(this.props.selectedLanguageKey)}products/${selectedProduct.previousProductId}/`
+      });
+    } : null;
+    const handleClickArrowRight = selectedProduct && selectedProduct.nextProductId ? () => {
+      this.setState({
+        redirect: `/${this.props.getLanguageSlug(this.props.selectedLanguageKey)}products/${selectedProduct.nextProductId}/`
+      });
+    } : null;
+    return selectedProduct
+      ? (<Modal onClickOutside={handleClickOutside} maxWidth="540px" onClickArrowLeft={handleClickArrowLeft} onClickArrowRight={handleClickArrowRight}>
+        <Product product={selectedProduct} fullscreen={true}/>
+      </Modal>)
+      : '';
+  }
+
+  getSelectedProduct(selectedProductId, selectedLanguageKey) {
+    let selectedProduct = null;
+    products.forEach((product, index) => {
+      const productId = convertToUrlFriendlyString(product.title)
+      if (productId === selectedProductId) {
+        selectedProduct = {
+          ...product,
+          previousProductId: index > 0 ? convertToUrlFriendlyString(products[index-1].title[selectedLanguageKey]) : null,
+          nextProductId: index < products.length-1 ? convertToUrlFriendlyString(products[index+1].title[selectedLanguageKey]) : null
+        }
+      }
+    });
+    return selectedProduct;
+  }
+
+  render() {
+    const selectedProductId = this.props.match && this.props.match.params && this.props.match.params.productId
+      ? this.props.match.params.productId
+      : null;
+    const selectedProduct = selectedProductId
+      ? this.getSelectedProduct(selectedProductId, this.props.selectedLanguageKey)
+      : null;
+
+    const listPage = {
+      title: {
+        en: 'Products | Dehli Musikk',
+        no: 'Produkter | Dehli Musikk'
+      },
+      heading: {
+        en: 'Products',
+        no: 'Produkter'
+      },
+      description: {
+        en: 'Products from Dehli Musikk',
+        no: 'Produkter fra Dehli Musikk'
+      }
+    }
+
+    const detailsPage = {
+      title: {
+        en: `${selectedProduct
+          ? selectedProduct.title.en
+          : ''} - Products | Dehli Musikk`,
+        no: `${selectedProduct
+          ? selectedProduct.title.no
+          : ''} - Innlegg | Dehli Musikk`
+      },
+      heading: {
+        en: selectedProduct
+          ? selectedProduct.title.en
+          : '',
+        no: selectedProduct
+          ? selectedProduct.title.no
+          : ''
+      },
+      description: {
+        en: selectedProduct
+          ? selectedProduct.content.en
+          : '',
+        no: selectedProduct
+          ? selectedProduct.content.no
+          : ''
+      }
+    }
+
+    let breadcrumbs = [
+      {
+        name: listPage.heading[this.props.selectedLanguageKey],
+        path: `/${this.props.getLanguageSlug(this.props.selectedLanguageKey)}products/`
+      }
+    ];
+    if (selectedProduct) {
+      breadcrumbs.push({
+        name: detailsPage.heading[this.props.selectedLanguageKey],
+        path: `/${this.props.getLanguageSlug(this.props.selectedLanguageKey)}products/${selectedProductId}/`
+      })
+    }
+
+    if (this.state.redirect) {
+      return <Redirect to={this.state.redirect}/>;
+    } else {
+      const selectedProductMultilingualIds = {
+        en: selectedProduct
+          ? convertToUrlFriendlyString(selectedProduct.title.en)
+          : '',
+        no: selectedProduct
+          ? convertToUrlFriendlyString(selectedProduct.title.no)
+          : ''
+      };
+      const metaTitle = selectedProduct
+        ? detailsPage.title[this.props.selectedLanguageKey]
+        : listPage.title[this.props.selectedLanguageKey];
+      const contentTitle = selectedProduct
+        ? detailsPage.heading[this.props.selectedLanguageKey]
+        : listPage.heading[this.props.selectedLanguageKey];
+      const metaDescription = selectedProduct
+        ? detailsPage.description[this.props.selectedLanguageKey]
+        : listPage.description[this.props.selectedLanguageKey];
+      return (<React.Fragment>
+        <Helmet htmlAttributes={{
+            lang: this.props.selectedLanguageKey
+          }}>
+          <title>{metaTitle}</title>
+          <meta name='description' content={metaDescription}/>
+          <link rel="canonical" href={`https://www.dehlimusikk.no/${this.props.getLanguageSlug(this.props.selectedLanguageKey)}products/${selectedProduct
+              ? selectedProductId + '/'
+              : ''}`}/>
+          <link rel="alternate" href={`https://www.dehlimusikk.no/products/${selectedProduct
+              ? selectedProductMultilingualIds.no + '/'
+              : ''}`} hreflang="no"/>
+          <link rel="alternate" href={`https://www.dehlimusikk.no/en/products/${selectedProduct
+              ? selectedProductMultilingualIds.en + '/'
+              : ''}`} hreflang="en"/>
+          <link rel="alternate" href={`https://www.dehlimusikk.no/products/${selectedProduct
+              ? selectedProductMultilingualIds.no + '/'
+              : ''}`} hreflang="x-default"/>
+          <meta property="og:title" content={contentTitle}/>
+          <meta property="og:url" content={`https://www.dehlimusikk.no/${this.props.getLanguageSlug(this.props.selectedLanguageKey)}products/${selectedProduct
+              ? selectedProductId + '/'
+              : ''}`}/>
+          <meta property="og:description" content={metaDescription}/>
+          <meta property="og:locale" content={this.props.selectedLanguageKey === 'en'
+              ? 'en_US'
+              : 'no_NO'}/>
+          <meta property="og:locale:alternate" content={this.props.selectedLanguageKey === 'en'
+              ? 'nb_NO'
+              : 'en_US'}/>
+          <meta property="twitter:title" content={contentTitle} />
+          <meta property="twitter:description" content={metaDescription} />
+        </Helmet>
+        <Container blur={selectedProduct !== null}>
+          <Breadcrumbs breadcrumbs={breadcrumbs}/>
+          <h1>{contentTitle}</h1>
+          <p>{
+              this.props.selectedLanguageKey === 'en'
+                ? 'Products from Dehli Musikk'
+                : 'Produkter fra Dehli Musikk'
+            }</p>
+        </Container>
+        {
+          selectedProduct ? this.renderSelectedProduct(selectedProduct) : ''
+        }
+        <Container blur={selectedProduct !== null}>
+          <List>
+            {this.renderProducts()}
+          </List>
+        </Container>
+      </React.Fragment>)
+    }
+  }
+}
+
+const mapStateToProps = state => ({selectedLanguageKey: state.selectedLanguageKey, location: state.router.location});
+
+const mapDispatchToProps = {
+  getLanguageSlug,
+  updateMultilingualRoutes,
+  updateSelectedLanguageKey
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Products);
