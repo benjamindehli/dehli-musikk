34d2fb3e1c3899006ae8335151a3fb13c54ff612
Benjamin Dehli <superelg@gmail.com>
2020-08-09 19:03:58 +0200

    Add YouTube time formatter

---
+++ src/helpers/timeFormatter.js
@@ -0,0 +1,16 @@
+export const youTubeTimeToSeconds = youTubeTime => {
+  var regex = /^PT(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?$/;
+  var hours = 0,
+    minutes = 0,
+    seconds = 0,
+    totalseconds;
+
+  if (regex.test(youTubeTime)) {
+    var matches = regex.exec(youTubeTime);
+    if (matches[1]) hours = Number(matches[1]);
+    if (matches[2]) minutes = Number(matches[2]);
+    if (matches[3]) seconds = Number(matches[3]);
+    totalseconds = hours * 3600 + minutes * 60 + seconds;
+  }
+  return totalseconds
+}
