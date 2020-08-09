4669d9fc756002937c254f88e1349467f48ba125
Benjamin Dehli <superelg@gmail.com>
2020-08-09 19:08:09 +0200

    Add component for list item video

---
+++ src/components/template/List/ListItem/ListItemVideo.js
@@ -0,0 +1,27 @@
+// Dependencies
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
+
+// Stylesheets
+import style from 'components/template/List/ListItem/ListItemVideo.module.scss';
+
+class ListItemThumbnail extends React.Component {
+  render() {
+    return (
+      <div className={style.videoContainer}>
+        <iframe width="945" height="532" src={`https://www.youtube.com/embed/${this.props.youTubeId}`} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen>
+        </iframe>
+      </div>
+    )
+  }
+};
+
+ListItemThumbnail.propTypes = {
+  youTubeId: PropTypes.string
+};
+
+ListItemThumbnail.defaultProps = {
+};
+
+export default ListItemThumbnail;
