6299829100947c5f98afe0561646f43d06866586
Benjamin Dehli <superelg@gmail.com>
2020-08-09 19:11:09 +0200

    Add partial component for latest videos

---
+++ src/components/partials/LatestVideos.js
@@ -0,0 +1,42 @@
+// Dependencies
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+// Components
+import Video from 'components/partials/Video';
+
+// Helpers
+import {convertToUrlFriendlyString} from 'helpers/urlFormatter'
+
+// Template
+import List from 'components/template/List';
+import ListItem from 'components/template/List/ListItem';
+
+// Data
+import {latestVideos} from 'data/videos';
+
+
+class LatestVideos extends Component {
+  renderVideos() {
+    return latestVideos && latestVideos.length
+      ? latestVideos.map(video => {
+        const videoId = convertToUrlFriendlyString(video.title[this.props.selectedLanguageKey]);
+        return (<ListItem key={videoId}>
+          <Video video={video}/>
+        </ListItem>)
+      })
+      : '';
+  }
+
+  render() {
+    return (<List>
+      {this.renderVideos()}
+    </List>)
+  }
+}
+
+const mapStateToProps = state => ({
+  selectedLanguageKey: state.selectedLanguageKey
+});
+
+export default connect(mapStateToProps, null)(LatestVideos);
