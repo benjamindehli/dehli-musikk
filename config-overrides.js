3af51bd59230e105723edc473d28a64a64e5a97a
Benjamin Dehli <superelg@gmail.com>
2020-10-28 22:41:14 +0100

    Adjust config override for new react-scripts version

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,5 @@
 module.exports = function override(config, env) {
   //do stuff with the webpack config...
-  config.module.rules[2].oneOf[0].options.limit = 0; // Disable base64 on small images
+  config.module.rules[1].oneOf[0].options.limit = 0; // Disable base64 on small images
   return config;
 }
