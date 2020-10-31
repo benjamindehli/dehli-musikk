e9a92e1124b287e528b0f0ecf0e8581e025272f8
Benjamin Dehli <superelg@gmail.com>
2020-10-31 16:59:09 +0100

    Adjust override for mini-css-extract-plugin

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,16 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = function override(config, env) {
   //do stuff with the webpack config...
-  config.plugins['MiniCssExtractPlugin'] = {
-    filename: '[name].css',
-    chunkFilename: '[id].css',
-    ignoreOrder: true, // Enable to remove warnings about conflicting order
-  }
+  config.plugins.forEach((plugin, i) => {
+    if (plugin instanceof MiniCssExtractPlugin) {
+      config.plugins.splice(i, 1, new MiniCssExtractPlugin({
+        filename: 'static/css/[name].[contenthash:8].css',
+        chunkFilename: 'static/css/[name].[contenthash:8].chunk.css',
+        ignoreOrder: true, // Enable to remove warnings about conflicting order
+      }));
+    }
+  });
   config.module.rules[1].oneOf[0].options.limit = 0; // Disable base64 on small images
   return config;
 }
