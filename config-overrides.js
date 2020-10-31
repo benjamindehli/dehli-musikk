8cab8a4faf581376ea8d9132a51f05f897067062
Benjamin Dehli <superelg@gmail.com>
2020-10-31 16:00:54 +0100

    Remove condition for config override

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,9 @@
 module.exports = function override(config, env) {
   //do stuff with the webpack config...
-  if (env === 'production'){
-    config.plugins['MiniCssExtractPlugin'] = {
-      filename: '[name].css',
-      chunkFilename: '[id].css',
-      ignoreOrder: true, // Enable to remove warnings about conflicting order
-    }
+  config.plugins['MiniCssExtractPlugin'] = {
+    filename: '[name].css',
+    chunkFilename: '[id].css',
+    ignoreOrder: true, // Enable to remove warnings about conflicting order
   }
   config.module.rules[1].oneOf[0].options.limit = 0; // Disable base64 on small images
   return config;
