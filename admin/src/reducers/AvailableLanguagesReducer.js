5fbf0292218c1ff0a803cfb3387972bffe7ed7b8
Benjamin Dehli <superelg@gmail.com>
2020-12-31 00:12:43 +0100

    Add reducer for available languages

---
+++ admin/src/reducers/AvailableLanguagesReducer.js
@@ -0,0 +1,17 @@
+const initialState = {
+    no: {
+      name: 'norsk',
+      path: ''
+    },
+    en: {
+      name: 'english',
+      path: 'en/'
+    }
+  };
+  
+  const reducer = (state = initialState) => {
+    return state;
+  };
+  
+  export default reducer;
+  
--- admin/src/reducers/index.js
+++ admin/src/reducers/index.js
@@ -4,6 +4,7 @@ import {connectRouter} from 'connected-react-router'
 
 // Reducers
 import AmplifiersReducer from 'reducers/AmplifiersReducer';
+import AvailableLanguagesReducer from 'reducers/AvailableLanguagesReducer';
 import EffectsReducer from 'reducers/EffectsReducer';
 import InstrumentsReducer from 'reducers/InstrumentsReducer';
 import PostsReducer from 'reducers/PostsReducer';
@@ -13,6 +14,7 @@ import VideosReducer from 'reducers/VideosReducer';
 
 const state = history => combineReducers({
   router: connectRouter(history),
+  availableLanguages: AvailableLanguagesReducer,
   amplifiers: AmplifiersReducer,
   effects: EffectsReducer,
   instruments: InstrumentsReducer,
