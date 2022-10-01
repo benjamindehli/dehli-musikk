10b9175bbd878b68ece6635782e3007dc441cbfa
Benjamin Dehli <superelg@gmail.com>
2022-10-01 19:41:08 +0200

    Handle "/" characters in generated URL slugs

--- src/helpers/urlFormatter.js
+++ src/helpers/urlFormatter.js
@@ -15,6 +15,7 @@ export const convertToUrlFriendlyString = string => {
     // Character replace
     string = replaceAndAddSpace(string, "&", "and");
     string = replaceAndAddSpace(string, "+", "plus");
+    string = replaceAndAddSpace(string, "/", "-");
     string = string.replace(/[æä]/g, "ae");
     string = string.replace(/[øö]/g, "oe");
     string = string.replace(/å/g, "aa");
