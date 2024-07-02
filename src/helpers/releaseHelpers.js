3ba1611f654d3e3f8a9aafea2c21f0fa91e10dbc
Benjamin Dehli <superelg@gmail.com>
2024-07-02 19:36:31 +0200

    Add release helpers

---
+++ src/helpers/releaseHelpers.js
@@ -0,0 +1,21 @@
+function getArtistNamesFromArtistNameString(artistNameString) {
+    return artistNameString.split(/[,&]/).map((artistName) => artistName.trim());
+}
+
+function getUniqueArtistNamesFromReleases(releases) {
+    const artistNames = [];
+    releases.forEach((release) => {
+        getArtistNamesFromArtistNameString(release.artistName).forEach((artistName) => artistNames.push(artistName));
+    });
+    return Array.from(new Set(artistNames)).sort();
+}
+
+export function getArtistNamesStringFromReleases(releases, languageKey) {
+    const uniqueArtistNames = getUniqueArtistNamesFromReleases(releases);
+    const locales = {
+        en: "en-GB",
+        no: "nb-NO"
+    };
+    const formatter = new Intl.ListFormat(locales[languageKey], { style: "long", type: "conjunction" });
+    return formatter.format(uniqueArtistNames);
+}
