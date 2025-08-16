1e56c10343f0b3726da949bd0ace32a84e17b030
Benjamin Dehli <superelg@gmail.com>
2025-08-16 18:32:09 +0200

    Adds date formatting utilities
    Adds helper functions for formatting dates in different formats and languages.
    
    Includes functions for:
    - Displaying dates in a user-friendly format (getPrettyDate), supporting both English and Norwegian.
    - Generating date strings suitable for rich snippets (getRichSnippetDateString).
    - Calculating a date one year in the future (getPlusOneYear).

--- src/helpers/dateFormatter.js
+++ src/helpers/dateFormatter.js
@@ -1,22 +1,52 @@
 const monthNames = {
-  en: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-  no: ['Januar', 'Februar', 'Mars', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Desember']
-}
+    en: [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ],
+    no: [
+        "Januar",
+        "Februar",
+        "Mars",
+        "April",
+        "Mai",
+        "Juni",
+        "Juli",
+        "August",
+        "September",
+        "Oktober",
+        "November",
+        "Desember"
+    ]
+};
 
 export const getPrettyDate = (date, language) => {
     const year = date.getFullYear();
     const month = monthNames[language][date.getMonth()];
     const day = date.getDate();
-    if (language === 'en'){
-      return `${month} ${day}, ${year}`;
-    }else {
-      return `${day}. ${month} ${year}`;
+    if (language === "en") {
+        return `${month} ${day}, ${year}`;
+    } else {
+        return `${day}. ${month} ${year}`;
     }
-}
+};
 
 export const getRichSnippetDateString = (date) => {
-  const year = date.getFullYear();
-  const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Months are 0 based, so we add 1
-  const day = date.getDate().toString().padStart(2, '0');
-  return `${year}-${month}-${day}`;
-}
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, "0"); // Months are 0 based, so we add 1
+    const day = date.getDate().toString().padStart(2, "0");
+    return `${year}-${month}-${day}`;
+};
+
+export const getPlusOneYear = () => {
+    return new Date(new Date().setFullYear(new Date().getFullYear() + 1)).toISOString();
+};
