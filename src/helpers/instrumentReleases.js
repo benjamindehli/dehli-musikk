b846e9e85db030c128b3862dbe25394f6d6bf551
Benjamin Dehli <superelg@gmail.com>
2025-10-15 19:56:57 +0200

    Adds helpers for equipment/product releases to release pages
    Adds functionality to fetch and display releases associated with specific equipment, distinguishing between instruments and products.
    
    Introduces methods to retrieve product releases and refactors existing instrument release logic for clarity and to accommodate the new product release functionality.

--- src/helpers/instrumentReleases.js
+++ src/helpers/instrumentReleases.js
@@ -10,10 +10,27 @@ const getRelease = releaseId => {
 }
 
 export const getInstrumentReleases = instrumentId => {
-  const instrumentReleaseConnections = releasesInstruments.filter(instrumentRelease => {
+  const instrumentReleaseConnections = releasesInstruments.filter(instrumentRelease => !instrumentRelease.isProduct).filter(instrumentRelease => {
     return instrumentRelease.equipmentId === instrumentId;
   })
   return instrumentReleaseConnections.map(instrumentReleaseConnection => {
-    return {...getRelease(instrumentReleaseConnection.releaseId), releaseId: instrumentReleaseConnection.releaseId };
+    const release = getRelease(instrumentReleaseConnection.releaseId);
+    if (!release) {
+      return null;
+    }
+    return {...release, releaseId: instrumentReleaseConnection.releaseId };
+  }).filter(release => release !== null); // Filter out any null values
+}
+
+export const getProductReleases = productId => {
+  const productReleaseConnections = releasesInstruments.filter(instrumentRelease => instrumentRelease.isProduct).filter(productRelease => {
+    return productRelease.equipmentId === productId;
   })
+  return productReleaseConnections.map(productReleaseConnection => {
+    const release = getRelease(productReleaseConnection.releaseId);
+    if (!release) {
+      return null;
+    }
+    return {...release, releaseId: productReleaseConnection.releaseId };
+  }).filter(release => release !== null); // Filter out any null values
 }
--- src/helpers/releaseInstruments.js
+++ src/helpers/releaseInstruments.js
@@ -1,5 +1,6 @@
 import releasesInstruments from 'data/releasesInstruments';
 import {instruments} from 'data/equipment';
+import products from 'data/products';
 import {convertToUrlFriendlyString} from 'helpers/urlFormatter'
 
 
@@ -9,11 +10,26 @@ const getInstrument = equipmentId => {
   })
 }
 
+const getProduct = equipmentId => {
+  return products.find(product => {
+    return convertToUrlFriendlyString(product.title) === equipmentId;
+  })
+}
+
 export const getReleaseInstruments = releaseId => {
-  const releaseInstrumentConnections = releasesInstruments.filter(releaseInstrument => {
+  const releaseInstrumentConnections = releasesInstruments.filter(instrumentRelease => !instrumentRelease.isProduct).filter(releaseInstrument => {
     return releaseInstrument.releaseId === releaseId;
   })
   return releaseInstrumentConnections.map(releaseInstrumentConnection => {
     return {...getInstrument(releaseInstrumentConnection.equipmentId), equipmentItemId: releaseInstrumentConnection.equipmentId };
+  })
+}
+
+export const getReleaseProducts = releaseId => {
+  const releaseProductConnections = releasesInstruments.filter(releaseInstrument => releaseInstrument.isProduct).filter(releaseProduct => {
+    return releaseProduct.releaseId === releaseId;
+  })
+  return releaseProductConnections.map(releaseProductConnection => {
+    return {...getProduct(releaseProductConnection.equipmentId), equipmentItemId: releaseProductConnection.equipmentId };
   })
 }
