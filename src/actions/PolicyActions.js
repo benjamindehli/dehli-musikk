9eaa3cd7baf5c6f5297df227ed7a7f3b178a7443
Benjamin Dehli <superelg@gmail.com>
2025-12-01 22:44:16 +0100

    Removes unused action and helper files
    Removes deprecated files related to policy actions,
    portfolio actions, and XML string formatting that are
    no longer in use by the application.

--- src/actions/PolicyActions.js
+++
@@ -1,5 +0,0 @@
-import {UPDATE_HAS_ACCEPTED_POLICY} from 'constants/types';
-
-export const updateHasAcceptedPolicy = (hasAcceptedPolicy) => dispatch => {
-  dispatch({type: UPDATE_HAS_ACCEPTED_POLICY, payload: hasAcceptedPolicy});
-}
