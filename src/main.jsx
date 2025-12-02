a967e495c8e422562d04fe990ee0d634d4e4d9f4
Benjamin Dehli <superelg@gmail.com>
2025-12-03 00:15:49 +0100

    Migrates to use Vite
    Updates the project to utilize Vite as the build tool, enabling faster development and improved performance.
    
    Removes the service worker and related configurations, as it is not currently needed for the application's functionality.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import * as serviceWorker from 'serviceWorker';
+//import * as serviceWorker from 'serviceWorker';
 import { hydrate, render } from "react-dom";
 
-import App from 'App';
+import App from './App.jsx';
 
-import 'style/styles.scss';
+import './style/styles.scss';
 
 
 const rootElement = document.getElementById("root");
@@ -17,4 +17,16 @@ if (rootElement.hasChildNodes()) {
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+//serviceWorker.register();
+
+/*
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import 'style/styles.scss';
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)*/
