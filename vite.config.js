b06fa040e7978a68a2172640430d4364ca3d0c16
Benjamin Dehli <superelg@gmail.com>
2025-12-03 00:14:35 +0100

    Configures Vite for project bundling
    Sets up Vite as the bundler, replacing the existing CRA setup.
    
    This change enables faster build times and improved development
    experience. It also configures the build output directory.

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import commonjs from "vite-plugin-commonjs";
+
+export default defineConfig({
+    plugins: [react(), svgr(), commonjs()],
+    build: {
+        outDir: "build" // CRA's default build output
+    }
+});
