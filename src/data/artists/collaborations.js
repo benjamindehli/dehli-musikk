029dbdb133849b59d2720d7f6eda393e7aedf54c
Benjamin Dehli <superelg@gmail.com>
2026-03-29 20:38:54 +0200

    Add artist identifiers and collaboration data
    Introduce structured data mapping artist names to their canonical MusicBrainz or Discogs IDs, and provide a breakdown for collaborative artist names.

---
+++ src/data/artists/collaborations.js
@@ -0,0 +1,16 @@
+const collaborations = [
+    {
+        name: "Bjørn Eidsvåg & Ylva",
+        artistNames: ["Bjørn Eidsvåg", "Ylva"]
+    },
+    {
+        name: 'Haunted By Silhouettes & Bjorn "Speed" Strid',
+        artistNames: ["Haunted By Silhouettes", 'Bjorn "Speed" Strid']
+    },
+    {
+        name: "Spinning Wheels Band, Randle Currie & Justin Williamson",
+        artistNames: ["Spinning Wheels Band", "Randle Currie", "Justin Williamson"]
+    }
+];
+
+export default collaborations;
---
+++ src/data/artists/jsonLdIds.js
@@ -0,0 +1,72 @@
+const artistJsonLdIds = [
+    {
+        name: "Aina Wassvik",
+        jsonLdId: "https://musicbrainz.org/artist/bb303849-21c4-40d9-b336-309b0dc38c3f"
+    },
+    {
+        name: "Audun Haukvik",
+        jsonLdId: "https://musicbrainz.org/artist/87fa6b87-4969-447c-afc3-eaeda1a7219e"
+    },
+    {
+        name: "Benjamin Dehli",
+        jsonLdId: "https://musicbrainz.org/artist/56639e59-2bb5-40bd-9d5a-97d964298b6f"
+    },
+    {
+        name: 'Bjorn "Speed" Strid',
+        jsonLdId: "https://musicbrainz.org/artist/fc2bf744-429b-487c-b93b-3f141b6c64aa"
+    },
+    {
+        name: "Bjørn Eidsvåg",
+        jsonLdId: "https://musicbrainz.org/artist/1e018877-f6d2-4487-8eb6-659fb389ada3"
+    },
+    {
+        name: "Changemakers",
+        jsonLdId: "https://www.discogs.com/artist/12021988-Changemakers"
+    },
+    {
+        name: "Esben Meyer",
+        jsonLdId: "https://musicbrainz.org/artist/436cc007-d7d9-4df7-812d-c0f77b2585bc"
+    },
+    {
+        name: "Grisene",
+        jsonLdId: "https://www.discogs.com/artist/564670-Grisene"
+    },
+    {
+        name: "Haunted By Silhouettes",
+        jsonLdId: "https://musicbrainz.org/artist/8759e49c-5994-4bee-89f8-f777bfe6f4c0"
+    },
+    {
+        name: "Justin Williamson",
+        jsonLdId: "https://musicbrainz.org/artist/bd5f6b4d-5eef-41d6-951e-c860dcbc1c98"
+    },
+    {
+        name: "KAASIN",
+        jsonLdId: "https://musicbrainz.org/artist/0fd2a19c-77dc-45ee-bc77-a7ad35ffa7f5"
+    },
+    {
+        name: "Randle Currie",
+        jsonLdId: "https://musicbrainz.org/artist/635875fc-4e2f-4880-a553-30f6b216b550"
+    },
+    {
+        name: "Set One's Cap",
+        jsonLdId: "https://musicbrainz.org/artist/90418515-6b90-465a-bacc-699f3c9b7297"
+    },
+    {
+        name: "Spinning Wheels Band",
+        jsonLdId: "https://musicbrainz.org/artist/a2a73b67-96cf-4411-a9de-8068d24e1d7e"
+    },
+    {
+        name: "Ståle Kaasin",
+        jsonLdId: "https://musicbrainz.org/artist/e69f8653-d0a6-479b-8bd3-35f643c3c4f9"
+    },
+    {
+        name: "Wild Whens",
+        jsonLdId: "https://musicbrainz.org/artist/85d95493-9bb7-4d7e-9629-cadf54057cb0"
+    },
+    {
+        name: "Ylva",
+        jsonLdId: "https://musicbrainz.org/artist/ca2dcad4-92cf-45dd-a4be-0cef893a94e6"
+    }
+];
+
+export default artistJsonLdIds;
