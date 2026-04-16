5198b19c750d04fbad17118652e3d68eb26ebf2a
Benjamin Dehli <superelg@gmail.com>
2026-04-16 19:16:52 +0200

    Use configuration files for Next

---
+++ next.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src')]
+  },
+  webpack(config) {
+    // Handle SVG imports as React components (SVGR)
+    const fileLoaderRule = config.module.rules.find(rule =>
+      rule.test && rule.test.toString().includes('svg')
+    );
+    if (fileLoaderRule) {
+      fileLoaderRule.exclude = /\.svg$/i;
+    }
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack']
+    });
+    return config;
+  }
+};
+
+module.exports = nextConfig;
