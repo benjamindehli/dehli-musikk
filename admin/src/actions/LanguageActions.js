a1730a1f51c9ced69c7eea1fc2072fc86cfc8708
Benjamin Dehli <superelg@gmail.com>
2026-04-18 17:52:28 +0200

    Remove admin app from repo

--- admin/src/actions/LanguageActions.js
+++
@@ -1,5 +0,0 @@
-export const getLanguageSlug = (selectedLanguageKey) => (dispatch, getState) => {
-  return getState().availableLanguages && getState().availableLanguages.hasOwnProperty(selectedLanguageKey)
-    ? getState().availableLanguages[selectedLanguageKey].path
-    : '';
-}
