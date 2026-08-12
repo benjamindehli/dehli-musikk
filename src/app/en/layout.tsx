c87c15b11f2b2ebf19e68a574b6bb1aae0e2afb3
Benjamin Dehli <superelg@gmail.com>
2026-08-12 17:58:37 +0200

    Preload the Roboto font

--- src/app/en/layout.tsx
+++ src/app/en/layout.tsx
@@ -8,6 +8,10 @@ export default function EnLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
             <head>
+                {/* crossOrigin is required even same-origin: fonts are always
+                    fetched in CORS mode, and without it the preload is not
+                    reused and the font downloads twice. */}
+                <link rel="preload" href="/fonts/Roboto.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
                 <link rel="preconnect" href="https://www.youtube.com" />
                 <link rel="preconnect" href="https://i.ytimg.com" />
                 <link rel="alternate" type="application/rss+xml" title="Dehli Musikk news" href="https://www.dehlimusikk.no/feed-en.rss" />
